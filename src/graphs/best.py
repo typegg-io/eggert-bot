c256cdd0b9839ca7b1e8b80313f7a9b17a961af9
Keegan <keegantournay@gmail.com>
2025-07-18 15:19:55 -0400

    fix: best graph properties

--- src/graphs/best.py
+++ src/graphs/best.py
@@ -1,14 +1,13 @@
 from graphs.core import plt, apply_theme
 
 def render(file_name: str, theme: dict, top_quotes: list[dict], title: str):
-    plt.rcParams.update({"font.size": 18})
-    ax = plt.subplots(figsize=(16, 9))[1]
+    ax = plt.subplots()[1]
 
     ranks = [i + 1 for i in range(len(top_quotes))]
     pp = [quote["pp"] for quote in top_quotes]
     ax.plot(ranks, pp)
 
-    ax.set_title(title, fontsize=100)
+    ax.set_title(title)
 
     apply_theme(ax, theme)
 
