1986ff03a65976d03bc5c24a2036ac41d76f7b01
Keegan <keegantournay@gmail.com>
2025-09-27 13:58:09 -0400

    feat: admin user checks

---
+++ src/commands/checks.py
@@ -0,0 +1,22 @@
+from discord.ext import commands
+
+from config import EIKO, KEEGAN
+from utils.errors import UserNotAdmin, UserNotOwner
+
+
+def is_bot_admin():
+    async def predicate(ctx: commands.Context):
+        if ctx.user["isAdmin"]:
+            return True
+        raise UserNotAdmin("Non admin user attempted to use an admin command")
+
+    return commands.check(predicate)
+
+
+def is_bot_owner():
+    async def predicate(ctx: commands.Context):
+        if ctx.user["userId"] in [EIKO, KEEGAN]:
+            return True
+        raise UserNotOwner("Non owner attempted to use an owner command")
+
+    return commands.check(predicate)
