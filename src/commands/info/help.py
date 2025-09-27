e44219f970daa02ab89bd9d68b4e41cc6cfba6da
Keegan <keegantournay@gmail.com>
2025-09-27 13:39:00 -0400

    refactor: update help command to display by category

--- src/commands/info/help.py
+++ src/commands/info/help.py
@@ -2,12 +2,13 @@ import importlib
 import os
 from typing import Optional
 
-from discord import Embed
 from discord.ext import commands
 
-from config import bot_prefix as prefix
-from database.users import get_user
-from utils import errors, files
+from commands.base import Command
+from config import BOT_PREFIX as prefix
+from utils import files
+from utils.errors import admin_command, unknown_command
+from utils.messages import Page, Message, Field
 
 info = {
     "name": "help",
@@ -16,56 +17,58 @@ info = {
     "parameters": "[command]",
 }
 
-async def setup(bot):
-    await bot.add_cog(Help(bot))
 
-class Help(commands.Cog):
-    def __init__(self, bot):
-        self.bot = bot
-
+class Help(Command):
     @commands.command(aliases=info["aliases"])
-    async def help(self, ctx, command: Optional[str] = None):
-        bot_user = get_user(ctx.author.id)
-
+    async def help(self, ctx: commands.Context, command: Optional[str] = None):
         if not command:
-            return await help_main(ctx, bot_user)
+            return await help_main(ctx)
 
-        return await help_command(ctx, bot_user, command)
+        return await help_command(ctx, command)
 
-async def help_main(ctx: commands.Context, bot_user: dict):
+
+async def help_main(ctx: commands.Context):
     description = (
-        f"Use `{prefix}help [command]` to get information about a specific command.\n\n"
+        f"Use `{prefix}help <command>` to get information about a specific command.\n\n"
         f"**Parameter Notation:**\n"
-        f"• Parameters in `[]` are optional\n"
         f"• Parameters in `<>` are required\n"
+        f"• Parameters in `[]` are optional\n"
         f"• Parameters with `|` indicate a choice between options"
     )
 
-    embed = Embed(
-        title="Command Help",
-        description=description,
-        color=bot_user["theme"]["embed"],
-    )
-
+    fields = []
     groups = files.get_command_groups()
     for group in groups:
+        if group == "admin" and not ctx.user["isAdmin"]:
+            continue
         command_list = []
         for file in os.listdir(f"./commands/{group}"):
             if file.endswith(".py") and not file.startswith("_") and not file.startswith("help"):
                 command_list.append(file[:-3])
         command_list.sort()
-        embed.add_field(
-            name=f"{group.title()} Commands",
-            value=", ".join(f"`{command}`" for command in command_list),
-            inline=False
-        )
+        fields.append(Field(
+            title=f"{group.title()}",
+            content=", ".join(f"`{command}`" for command in command_list),
+        ))
 
-    embed.set_footer(text="Developed by @keegant", icon_url="https://cdn.discordapp.com/avatars/155481579005804544/33ede24295683bbb2253481d5029266e.webp?size=1024")
+    page = Page(
+        title="Command Help",
+        description=description,
+        fields=fields,
+        color=ctx.user["theme"]["embed"],
+    )
 
-    await ctx.send(embed=embed)
+    message = Message(
+        ctx=ctx,
+        page=page,
+        footer="Developed by @keegant",
+        footer_icon="https://cdn.discordapp.com/avatars/155481579005804544/33ede24295683bbb2253481d5029266e.webp?size=1024",
+    )
+
+    await message.send()
 
 
-async def help_command(ctx: commands.Context, bot_user: dict, command_name: str):
+async def help_command(ctx: commands.Context, command_name: str):
     groups = files.get_command_groups()
     command = None
     for group in groups:
@@ -74,20 +77,17 @@ async def help_command(ctx: commands.Context, bot_user: dict, command_name: str)
                 module = importlib.import_module(f"commands.{group}.{file[:-3]}")
                 command_info = module.info
                 if command_name in [command_info["name"]] + command_info["aliases"]:
+                    if group == "admin" and not ctx.user["isAdmin"]:
+                        return await ctx.send(embed=admin_command())
                     command = command_info
                     break
 
     if not command:
-        return await ctx.send(embed=errors.unknown_command())
+        return await ctx.send(embed=unknown_command())
 
     name = command["name"]
     aliases = command["aliases"]
-
-    embed = Embed(
-        title=f"Help: `{prefix}{name}`",
-        description=command["description"],
-        color=bot_user["theme"]["embed"]
-    )
+    fields = []
 
     if "parameters" in command:
         parameter_string = f"`{prefix}{name}"
@@ -97,24 +97,33 @@ async def help_command(ctx: commands.Context, bot_user: dict, command_name: str)
         if "defaults" in command:
             for param, default in command["defaults"].items():
                 parameter_string += f"\n`{param}` defaults to {default}"
-        embed.add_field(
-            name="Usage",
-            value=parameter_string,
-            inline=False,
-        )
+        fields.append(Field(
+            title="Usage",
+            content=parameter_string,
+        ))
 
     if "usage" in command:
-        embed.add_field(
-            name="Usage",
-            value="\n".join([f"`{prefix}{usage}`" for usage in command['usage']]),
-            inline=False,
-        )
+        fields.append(Field(
+            title="Usage",
+            content="\n".join([f"`{prefix}{usage}`" for usage in command["usage"]]),
+        ))
 
     if aliases:
-        embed.add_field(
-            name="Aliases",
-            value=", ".join([f"`{prefix}{alias}`" for alias in aliases]),
-            inline=False
-        )
+        fields.append(Field(
+            title="Aliases",
+            content=", ".join([f"`{prefix}{alias}`" for alias in aliases]),
+        ))
 
-    await ctx.send(embed=embed)
+    page = Page(
+        title=f"Help: `{prefix}{name}`",
+        description=command["description"],
+        fields=fields,
+        color=ctx.user["theme"]["embed"],
+    )
+
+    message = Message(
+        ctx=ctx,
+        page=page,
+    )
+
+    await message.send()
