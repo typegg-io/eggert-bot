e725ba7056d31a98de000d337a39de9e22ff833e
Keegan <keegantournay@gmail.com>
2025-09-29 17:32:40 -0400

    style: about message new line

--- src/commands/info/about.py
+++ src/commands/info/about.py
@@ -20,8 +20,8 @@ class About(Command):
             title="Eggert",
             description=(
                 f""
-                f"{self.bot.user.mention} is a typing statistics Discord bot. It is designed to enhance the "
-                f"<:typegg_logo:1421559259929382923> TypeGG experience, providing detailed insights and features.\n\n"
+                f"{self.bot.user.mention} is a typing statistics Discord bot. It is designed to enhance\n"
+                f"the <:typegg_logo:1421559259929382923> TypeGG experience, providing detailed insights and features.\n\n"
                 f"Developed by <@{KEEGAN}>, written in <:python_logo:1219588087383064647> Python.\n"
                 f"For source code and contributing, visit the <:github:1421565431533273269> "
                 f"[GitHub Repository](https://github.com/typegg-io/eggert-bot)\n\n"
