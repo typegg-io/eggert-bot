123b5129dd419dbcbd70e711634bf104a5e7757b
Keegan <keegantournay@gmail.com>
2025-09-30 15:26:13 -0400

    fix: worst command not importing or filtering metrics

--- src/commands/stats/worst.py
+++ src/commands/stats/worst.py
@@ -3,7 +3,8 @@ from typing import Optional
 from discord.ext import commands
 
 from commands.base import Command
-from commands.stats.best import run
+from commands.stats.best import run, metrics
+from utils.errors import invalid_argument
 
 info = {
     "name": "worst",
@@ -17,5 +18,9 @@ info = {
 class Worst(Command):
     @commands.command(aliases=info["aliases"])
     async def worst(self, ctx, username: Optional[str] = "me", metric: Optional[str] = "pp"):
+        if metric not in metrics:
+            return await ctx.send(embed=invalid_argument(metrics))
+
         profile = await self.get_profile(ctx, username, races_required=True)
-        await run(ctx, profile, metric, reverse=False)
+        await self.import_user(ctx, profile)
+        await run(ctx, profile, metric)
