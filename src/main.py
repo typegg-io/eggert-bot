5c772b4c3fa2f8924a728f1ed7d4274db2289b0d
Keegan <keegantournay@gmail.com>
2025-09-30 20:08:27 -0400

    fix: add daily quote results to bot tasks

--- src/main.py
+++ src/main.py
@@ -10,7 +10,7 @@ from watchdog.observers import Observer
 from commands.base import Command
 from config import BOT_PREFIX, BOT_TOKEN, STAGING, STATS_CHANNEL_ID, SOURCE_DIR, DAILY_QUOTE_CHANNEL_ID
 from database.bot.users import get_user_ids, get_all_command_usage, update_commands
-from tasks import daily_quote_ping
+from tasks import daily_quote_ping, daily_quote_results
 from utils import dates
 from utils.files import get_command_modules
 from utils.logging import get_log_message, log
@@ -77,6 +77,7 @@ async def on_ready():
 async def tasks():
     now = dates.now()
     if now.hour == 0 and now.minute == 5:
+        await daily_quote_results(bot)
         await daily_quote_ping(bot)
 
 
