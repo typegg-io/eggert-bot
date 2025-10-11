bb916e0acd113d58609dbacb3b3e8313a99a5750
Keegan <keegantournay@gmail.com>
2025-10-11 15:50:23 -0400

    refactor: main code cleanup

--- src/main.py
+++ src/main.py
@@ -18,69 +18,91 @@ from utils.messages import welcome_message, command_milestone
 from watcher import ReloadHandler
 from web_server import start_web_server
 
+# Bot setup
 intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True
 intents.guilds = True
-bot = commands.Bot(command_prefix=BOT_PREFIX, case_insensitive=True, intents=intents)
+
+bot = commands.Bot(
+    command_prefix=BOT_PREFIX,
+    case_insensitive=True,
+    intents=intents,
+)
 bot.remove_command("help")
 
-total_commands = sum(get_all_command_usage().values())
+# Globals
 users = get_user_ids()
+total_commands = sum(get_all_command_usage().values())
 
 
+# Event handlers
 @bot.event
-async def on_message(message):
-    if message.content.startswith(BOT_PREFIX) and not message.author.bot and not STAGING:
+async def on_message(message: discord.Message):
+    if (
+        message.content.startswith(BOT_PREFIX)
+        and not message.author.bot
+        and not STAGING
+    ):
         log_message = get_log_message(message)
         log(log_message)
-        user_id = message.author.id
-        if user_id not in users:
-            users.append(user_id)
+
+        if message.author.id not in users:
+            users.append(message.author.id)
             return await message.reply(content=welcome_message)
 
     await bot.process_commands(message)
 
 
 @bot.event
-async def on_command_completion(ctx):
+async def on_command_completion(ctx: commands.Context):
     global total_commands
+
     command_origin = "server" if ctx.guild else "dm"
     update_commands(ctx.author.id, ctx.command.name, command_origin)
+
     total_commands += 1
     if total_commands % 50_000 == 0:
         await celebrate_milestone(ctx, total_commands)
 
 
-async def celebrate_milestone(ctx, milestone):
+async def celebrate_milestone(ctx: commands.Context, milestone: int):
     channel = bot.get_channel(STATS_CHANNEL_ID)
-    await channel.send(embed=command_milestone(ctx.author.id, milestone))
+    if channel:
+        await channel.send(embed=command_milestone(ctx.author.id, milestone))
 
 
+# Lifecycle
 @bot.event
 async def on_ready():
     await load_commands()
     await bot.load_extension("error_handler")
+
     if not STAGING:
-        tasks.start()
+        periodic_tasks.start()
         await start_web_server(bot)
     else:
         loop = asyncio.get_running_loop()
         start_watcher(bot, loop)
+
     log("Bot ready.")
 
 
+# Background tasks
 @tasks.loop(minutes=1)
-async def tasks():
+async def periodic_tasks():
     now = dates.now()
+
     if now.hour == 20 and now.minute == 0:
         await daily_quote_reminder(bot)
+
     elif now.hour == 0 and now.minute == 5:
         await daily_quote_results(bot)
         await daily_quote_ping(bot)
         await import_daily_quotes()
 
 
+# Utilities
 async def load_commands():
     for group, file, module in get_command_modules():
         for obj in module.__dict__.values():
@@ -97,11 +119,14 @@ def start_watcher(bot, loop):
 
 
 def clear_image_cache():
-    images = glob.glob("*.png")
-    for file in images:
-        os.remove(file)
+    for file in glob.glob("*.png"):
+        try:
+            os.remove(file)
+        except Exception:
+            pass
 
 
+# Entry point
 if __name__ == "__main__":
     clear_image_cache()
     bot.run(BOT_TOKEN)
