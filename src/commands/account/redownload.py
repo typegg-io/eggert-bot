bd19f887084e2c77b4445c789e0516a6df2d3082
Keegan <keegantournay@gmail.com>
2025-10-30 12:25:01 -0400

    fix: limit reimports to self (except for admins)

--- src/commands/account/redownload.py
+++ src/commands/account/redownload.py
@@ -6,6 +6,7 @@ from commands.account.download import run as download
 from commands.base import Command
 from database.typegg.races import get_latest_race, delete_races
 from utils.colors import ERROR, WARNING
+from utils.logging import ADMIN_ALIASES
 from utils.messages import Page, Message
 from utils.strings import escape_formatting
 
@@ -23,32 +24,51 @@ class ReDownload(Command):
     @commands.cooldown(1, 600, commands.BucketType.user)
     @commands.command(aliases=info["aliases"])
     async def redownload(self, ctx, username: Optional[str] = "me"):
-        profile = await self.get_profile(ctx, username, races_required=True)
-        user_id = profile["userId"]
-        latest_race = get_latest_race(user_id)
-        if not latest_race:
+        try:
+            is_admin = ctx.author.id in ADMIN_ALIASES.keys()
+            profile = await self.get_profile(ctx, username, races_required=True)
+            user_id = profile["userId"]
+
+            if not is_admin and user_id != ctx.user["userId"]:
+                ctx.command.reset_cooldown(ctx)
+                message = Message(ctx, Page(
+                    title="User Limit",
+                    description="You can only re-import your own races",
+                    color=ERROR,
+                ))
+                return await message.send()
+
+            latest_race = get_latest_race(user_id)
+            if not latest_race:
+                ctx.command.reset_cooldown(ctx)
+                message = Message(ctx, Page(
+                    title="Not Imported",
+                    description="Your account is not yet imported",
+                    color=ERROR,
+                ))
+                return await message.send()
+
             message = Message(ctx, Page(
-                title="Not Imported",
-                description="Your account is not yet imported",
-                color=ERROR,
+                title="Are You Sure?",
+                description=f"Please type \"confirm\" to proceed with the re-import",
+                color=WARNING,
             ))
-            return await message.send()
+            await message.send()
 
-        message = Message(ctx, Page(
-            title="Are You Sure?",
-            description=f"Please type \"confirm\" to proceed with the re-import",
-            color=WARNING,
-        ))
-        await message.send()
+            if not await self.await_confirmation(ctx):
+                return
 
-        if not await self.await_confirmation(ctx):
-            return
+            delete_races(user_id)
+            message = Message(ctx, Page(
+                title="Races Deleted",
+                description=f"Deleted all races for {escape_formatting(profile["username"])}"
+            ))
+            await message.send()
 
-        delete_races(user_id)
-        message = Message(ctx, Page(
-            title="Races Deleted",
-            description=f"Deleted all races for {escape_formatting(profile["username"])}"
-        ))
-        await message.send()
+            await download(ctx, profile)
 
-        await download(ctx, profile)
+            if is_admin:
+                await ctx.command.reset_cooldown(ctx)
+        except Exception as e:
+            ctx.command.reset_cooldown(ctx)
+            raise e
