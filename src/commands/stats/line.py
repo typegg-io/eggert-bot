7eda64a93e8471a235f51581016af48bd6b33444
Keegan <keegantournay@gmail.com>
2025-11-06 03:18:19 -0500

    feat: line graph command

---
+++ src/commands/stats/line.py
@@ -0,0 +1,185 @@
+import bisect
+from typing import Optional
+
+from discord import File
+from discord.ext import commands
+
+from commands.base import Command
+from database.typegg.races import get_races
+from graphs import line
+from utils.strings import get_argument
+
+metrics = {
+    "pp": {
+        "columns": "pp quoteId",
+        "title": "Total pp",
+    },
+    "best": {
+        "columns": "pp",
+        "title": "Best pp",
+    },
+    "wpm": {
+        "columns": "wpm",
+        "title": "Best WPM",
+    },
+    "races": {
+        "columns": "raceNumber",
+        "title": "Races",
+    },
+    "quotes": {
+        "columns": "quoteId",
+        "title": "Quotes Typed",
+    },
+    "characters": {
+        "columns": "wpm duration",
+        "title": "Characters Typed",
+    }
+}
+
+max_users = 5
+
+info = {
+    "name": "linegraph",
+    "aliases": ["lg", "l"],  # -rl, etc. (ignore metric as param)
+    "description": f"Displays a line graph of up to {max_users} users for a given metric",
+    "parameters": f"<metric> [username1] [username2] ... [username{max_users}]",
+}
+
+
+class LineGraph(Command):
+    @commands.command(aliases=info["aliases"])
+    async def linegraph(self, ctx, metric: str, username1: Optional[str] = "me", *other_users: Optional[str]):
+        metric = get_argument(metrics.keys(), metric)
+        other_users = other_users[:max_users - 1]
+        usernames = set(other_users)
+        usernames.add(username1)
+        profiles = []
+        for username in usernames:
+            profile = await self.get_profile(ctx, username, races_required=True)
+            profiles.append(profile)
+            await self.import_user(ctx, profile)
+        await run(ctx, metric, profiles)
+
+
+def get_total_pp_over_time(race_list: list[dict]):
+    quote_bests = {}
+    best_pps = []
+    total_pp = []
+    current_total = 0.0
+    max_entries = 250
+
+    for race in race_list:
+        quote_id = race["quoteId"]
+        pp = race["pp"]
+        old_best = quote_bests.get(quote_id)
+
+        if old_best is None or pp > old_best["pp"]:
+            if old_best is not None:
+                old_pp = old_best["pp"]
+                idx = bisect.bisect_left([-x for x in best_pps], -old_pp)
+                if idx < len(best_pps) and best_pps[idx] == old_pp:
+                    best_pps.pop(idx)
+
+            bisect.insort_left(best_pps, pp)
+            best_pps.sort(reverse=True)
+
+            quote_bests[quote_id] = race
+
+            if len(best_pps) <= max_entries or best_pps.index(pp) < max_entries:
+                current_total = 0.0
+                for i, p in enumerate(best_pps[:max_entries]):
+                    current_total += p * 0.97 ** i
+
+        total_pp.append(current_total)
+
+    return total_pp
+
+
+def get_best_over_time(race_list: list[dict], key: str):
+    values = []
+    best = race_list[0][key]
+
+    for race in race_list:
+        value = race[key]
+        if value > best:
+            best = value
+        values.append(best)
+
+    return values
+
+
+def get_quotes_over_time(race_list: list[dict]):
+    quotes_typed = []
+    unique_quotes = set()
+
+    for race in race_list:
+        unique_quotes.add(race["quoteId"])
+        quotes_typed.append(len(unique_quotes))
+
+    return quotes_typed
+
+
+def get_characters_over_time(race_list: list[dict]):
+    characters_typed = []
+    total = 0
+
+    for race in race_list:
+        quote_length = int(round(race["wpm"] * race["duration"] / 12000) + 1)
+        total += quote_length
+        characters_typed.append(total)
+
+    return characters_typed
+
+
+async def run(ctx: commands.Context, metric: str, profiles: list[dict]):
+    profiles.sort(key=lambda x: -x["stats"]["races"])
+    username = profiles[0]["username"]
+
+    lines = []
+    for profile in profiles:
+        columns = metrics[metric]["columns"].split(" ")
+        columns.append("timestamp")
+        min_pp = 0.01 if metric in ["wpm", "quotes"] else 0
+
+        race_list = await get_races(
+            profile["userId"],
+            columns,
+            min_pp=min_pp,
+            order_by="timestamp",
+        )
+
+        x_values = [race["timestamp"] for race in race_list]
+        y_values = []
+
+        if metric == "pp":
+            y_values = get_total_pp_over_time(race_list)
+        elif metric in ["best", "wpm"]:
+            y_values = get_best_over_time(race_list, key=columns[0])
+        elif metric == "races":
+            y_values = [race["raceNumber"] for race in race_list]
+        elif metric == "quotes":
+            y_values = get_quotes_over_time(race_list)
+        elif metric == "characters":
+            y_values = get_characters_over_time(race_list)
+
+        lines.append({
+            "username": profile["username"],
+            "x_values": x_values,
+            "y_values": y_values,
+        })
+
+        if profile["userId"] == ctx.user["userId"]:
+            username = profile["username"]
+
+    lines.sort(key=lambda x: -x["y_values"][-1])
+
+    title = metrics[metric]["title"]
+    file_name = line.render(
+        username,
+        lines,
+        f"{title} Over Time",
+        title,
+        ctx.user["theme"],
+    )
+    file = File(file_name, filename=file_name)
+    await ctx.send(file=file)
---
+++ src/graphs/line.py
@@ -0,0 +1,62 @@
+from matplotlib.ticker import FuncFormatter
+
+from graphs.core import plt, apply_theme, interpolate_segments, apply_date_ticks, generate_file_name
+from utils.dates import get_timestamp_list
+from utils.strings import format_big_number
+
+
+def render(
+    author_username: str,
+    lines: list[dict],
+    title: str,
+    x_label: str,
+    theme: dict,
+):
+    fig, ax = plt.subplots()
+
+    themed_line = 0
+    timestamps = get_timestamp_list([timestamp for line in lines for timestamp in line["x_values"]])
+    max_timestamp = max(timestamps)
+
+    for i, line in enumerate(lines):
+        username = line["username"]
+        x = line["x_values"]
+        y = line["y_values"]
+
+        # Extending lines to the end of the graph
+        y.append(y[-1])
+        x = get_timestamp_list(x)
+        x.append(max_timestamp)
+
+        # Downsampling
+        first_x, first_y = x[0], y[0]
+        last_x, last_y = x[-1], y[-1]
+        if len(x) > 10000:
+            x = x[::len(x) // 1000]
+            y = y[::len(y) // 1000]
+        x.insert(0, first_x)
+        x.append(last_x)
+        y.insert(0, first_y)
+        y.append(last_y)
+
+        x, y = interpolate_segments(x, y)
+        ax.plot(x, y, label=username)
+
+        if username == author_username:
+            themed_line = i
+
+    plt.grid()
+    ax.set_title(title)
+    ax.set_xlabel(x_label)
+    ax.set_ylabel("Dates")
+
+    apply_date_ticks(ax, timestamps)
+    ax.yaxis.set_major_formatter(FuncFormatter(format_big_number))
+
+    apply_theme(ax, theme, themed_line=themed_line)
+
+    file_name = generate_file_name("line")
+    plt.savefig(file_name)
+    plt.close(fig)
+
+    return file_name
