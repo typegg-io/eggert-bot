f055cf2c885253691f57e273784c4a50666a29e8
Keegan <keegantournay@gmail.com>
2026-01-09 15:02:52 -0500

    feat(linegraph): add nwpm

--- src/utils/stats.py
+++ src/utils/stats.py
@@ -1,6 +1,7 @@
 def calculate_total_pp(quote_bests: list[dict]):
     """Returns the total performance given a list of quote bests."""
-    return sum(q["pp"] * (0.97 ** i) for i, q in enumerate(quote_bests))
+    quote_bests.sort(key=lambda x: -x["pp"])
+    return sum(q["pp"] * (0.97 ** i) for i, q in enumerate(quote_bests[:250]))
 
 
 def calculate_quote_bests(race_list: list[dict]):
