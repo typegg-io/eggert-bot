02491ba4ced2bd43f4f0d8755c380bbe0cac21f1
Keegan <keegantournay@gmail.com>
2026-02-10 18:45:07 -0500

    feat(bestaverages): display race details of top average

--- src/commands/performance/bestaverages.py
+++ src/commands/performance/bestaverages.py
@@ -3,11 +3,12 @@ from typing import Optional
 from discord.ext import commands
 
 from commands.base import Command
+from database.typegg.quotes import get_quotes
 from database.typegg.races import get_races
 from utils.dates import parse_date
 from utils.errors import NumberGreaterThan, NotEnoughRaces
 from utils.messages import Page, Message
-from utils.strings import get_flag_title, date_range_display, parse_number
+from utils.strings import get_flag_title, date_range_display, parse_number, discord_date
 
 info = {
     "name": "bestaverages",
@@ -35,11 +36,12 @@ async def run(ctx: commands.Context, profile: dict, n: int):
         raise NumberGreaterThan
 
     ctx.flags.gamemode = ctx.flags.gamemode or "quickplay"
+    flag_title = get_flag_title(ctx.flags)
     ctx.flags.status = ctx.flags.status or "ranked"
 
     race_list = await get_races(
         user_id=profile["userId"],
-        columns=["wpm", "raceNumber", "timestamp"],
+        columns=["wpm", "raceNumber", "timestamp", "accuracy", "pp", "quoteId"],
         flags=ctx.flags,
     )
 
@@ -72,7 +74,9 @@ async def run(ctx: commands.Context, profile: dict, n: int):
 
     best_averages.sort(key=lambda x: x[0], reverse=True)
 
+    top_average_desc = ""
     description = ""
+
     for rank, (average, start_index) in enumerate(best_averages, 1):
         start_race = race_list[start_index]
         end_race = race_list[start_index + n - 1]
@@ -88,14 +92,54 @@ async def run(ctx: commands.Context, profile: dict, n: int):
             f"{f"#{end_number:,}" if end_number else "DNF"})\n\n"
         )
 
-    page = Page(
-        title=f"Best Last {n:,} Averages" + get_flag_title(ctx.flags),
+        if not top_average_desc:
+            top_average_desc += description
+
+    pages = [Page(
+        title=f"Best Last {n:,} Averages" + flag_title,
         description=description if description else "No averages found",
-    )
+        button_name="Best Averages",
+    )]
+
+    if best_averages and ctx.flags.gamemode in ["quickplay", "lobby"]:
+        top_average, top_start_index = best_averages[0]
+
+        start_offset = max(0, n - 25)  # If n > 25, start from the last 25 races
+
+        top_races = race_list[top_start_index + start_offset:top_start_index + n]
+        quote_list = get_quotes()
+
+        race_descriptions = ""
+        for race in top_races:
+            if race["wpm"] == 0:
+                desc = "DNF - "
+            else:
+                desc = (
+                    f"{race["wpm"]:,.2f} WPM - "
+                    f"{race["accuracy"]:.2%} - " +
+                    (f"{race["pp"]:,.2f} pp - " if race["pp"] > 0 else "")
+                )
+
+            desc += (
+                f"{quote_list[race["quoteId"]]["difficulty"]:.2f}★ - "
+                f"{discord_date(race["timestamp"])} - "
+                f"[<:quote:1470361772132143277>](https://typegg.io/solo/{race["quoteId"]})\n"
+            )
+
+            race_descriptions += desc
+
+        if n > 25:
+            race_descriptions += f"+{n - 25:,} others"
+
+        pages.append(Page(
+            title=f"Top Average of {n:,}" + flag_title,
+            description=f"{top_average_desc}**Races:**\n" + race_descriptions,
+            button_name="Top Average"
+        ))
 
     message = Message(
         ctx,
-        page=page,
+        pages=pages,
         profile=profile,
     )
 
