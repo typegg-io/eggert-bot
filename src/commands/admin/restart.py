c73bfac881b512173f2a5672637912f6f5317337
Keegan <keegantournay@gmail.com>
2026-05-29 23:45:45 -0400

    feat: add restart command

---
+++ src/commands/admin/restart.py
@@ -0,0 +1,36 @@
+import subprocess
+
+from discord import Embed
+from discord.ext import commands
+
+from bot_setup import BotContext
+from commands.base import Command
+from commands.checks import is_bot_owner
+from config import STAGING
+from utils.messages import Page, Message
+
+info = {
+    "name": "restart",
+    "aliases": [],
+    "description": "Restarts the bot process.",
+}
+
+
+class Restart(Command):
+    ignore_flags = True
+
+    @commands.command(aliases=info["aliases"])
+    @is_bot_owner()
+    async def restart(self, ctx: BotContext):
+        if STAGING:
+            return
+
+        message = Message(ctx, Page(title="Restarting..."))
+        await message.send()
+
+        await message.message.edit(embed=Embed(title="Restarted"))
+
+        subprocess.Popen(
+            ["sudo", "systemctl", "restart", "eggert-bot"],
+            start_new_session=True,
+        )
